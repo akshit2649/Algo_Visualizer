05829e7c7c558b639c5bd208ccd9d8431522b231
Akshit Thakur <akshit2649@gmail.com>
2022-10-11 01:02:32 +0530

    home done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="menu">
+      <div className="menuContainer">
+        <h1 className="title">Choose an Algorithm</h1>
+        <div className="choises">
+          <Link to="/pathfind" className="pathfinding">
+            PathFinding Algorithm
+          </Link>
+          <Link to="/sorting" className="sorting">
+            Sorting Algorithm
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
